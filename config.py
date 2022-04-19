4e45f770f7574bc8ead4c3391d651b1861b00d87
maritzamiranda <maritzageraldina@gmail.com>
2022-04-19 15:50:35 -0600

    last

--- config.py
+++ config.py
@@ -1,8 +1,7 @@
 class BaseConfig(object):
-    # SQLALCHEMY_DATABASE_URI = "mysql://root:root@localhost:3306/compras_ventas"
-    SQLALCHEMY_DATABASE_URI = "sqlite:///justin.db"
+    SQLALCHEMY_DATABASE_URI = "mysql://b03f89f8059b92:9f173f72@us-cdbr-east-05.cleardb.net/heroku_593e32d002747d8"
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    # SQLALCHEMY_POOL_SIZE = 20
-    # SQLALCHEMY_POOL_TIMEOUT = 300
+    SQLALCHEMY_POOL_SIZE = 20
+    SQLALCHEMY_POOL_TIMEOUT = 300
     SECRET_KEY = "horrible_secret_key"
     LOGIN_DISABLED = True
