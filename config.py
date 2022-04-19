94f1cf70a91ca021eea87f8ea0725ad4a00e7ea3
maritzamiranda <maritzageraldina@gmail.com>
2022-04-19 15:01:23 -0600

    ready

--- config.py
+++ config.py
@@ -1,7 +1,8 @@
 class BaseConfig(object):
-    SQLALCHEMY_DATABASE_URI = "mysql://root:root@localhost:3306/compras_ventas"
+    # SQLALCHEMY_DATABASE_URI = "mysql://root:root@localhost:3306/compras_ventas"
+    SQLALCHEMY_DATABASE_URI = "sqlite:///justin.db"
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    SQLALCHEMY_POOL_SIZE = 20
-    SQLALCHEMY_POOL_TIMEOUT = 300
+    # SQLALCHEMY_POOL_SIZE = 20
+    # SQLALCHEMY_POOL_TIMEOUT = 300
     SECRET_KEY = "horrible_secret_key"
     LOGIN_DISABLED = True
